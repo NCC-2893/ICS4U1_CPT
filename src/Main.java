db883ad2e5a99c04243aec8a30b91d245c2fc0ef
Marcus Moreria <49823652+NCC-177358@users.noreply.github.com>
2021-11-07 20:30:03 -0500

    CPT V0.1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,9 @@
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+        Velocity vel = new Velocity(100,3.6);
+        System.out.println(vel.timeAtIntercept());
+        System.out.println(vel.distanceAtIntercept());
+        System.out.println(vel.speedAtIntercept());
     }
 }
--- src/Velocity.java
+++ src/Velocity.java
@@ -1,3 +1,48 @@
 public class Velocity {
+    private double constantSpeed;
+    private double accelerationInSeconds;
+
+    public Velocity(double constantSpeed, double acceleration){
+        this.constantSpeed = constantSpeed;
+        this.accelerationInSeconds = acceleration;
+    }
+
+    public double getConstantSpeed() {
+        return constantSpeed;
+    }
+
+    public void setConstantSpeed(double constantSpeed) {
+        this.constantSpeed = constantSpeed;
+    }
+
+    public double getAcceleration() {
+        return accelerationInSeconds;
+    }
+
+    public void setAcceleration(double accelerationInSeconds) {
+        this.accelerationInSeconds = accelerationInSeconds;
+    }
+
+    private double halfOfAcceleration(){
+        return 0.5 * this.accelerationInSeconds;
+    }
     
+    private double divideConstantSpeedByAcceleration(){
+        return this.constantSpeed/this.accelerationInSeconds;
+    }
+
+    public double timeAtIntercept(){
+        double time = divideConstantSpeedByAcceleration()/halfOfAcceleration();
+        return time;
+    }
+
+    public double distanceAtIntercept(){
+        return divideConstantSpeedByAcceleration() * timeAtIntercept();
+    }
+
+    public double speedAtIntercept(){
+        double speedMS = this.accelerationInSeconds * timeAtIntercept();
+        double speedKH = speedMS * 3.6;
+        return speedKH;
+    }
 }
