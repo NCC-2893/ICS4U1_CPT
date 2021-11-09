21305cd7a899c54b413d341e0f1531b7b7f9f37e
Marcus Moreria <49823652+NCC-177358@users.noreply.github.com>
2021-11-08 20:25:45 -0500

    CPT V0.4

---
+++ src/SortArray.java
@@ -0,0 +1,20 @@
+/**
+ * Marcus Moreira
+ * 11/11/2021
+ * SortArray.java
+ */
+
+public class SortArray {
+    public static void bubbleSort(Velocity[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length-1; j++ ) {
+				if (arr[j].getConstantSpeed() > arr[j+1].getConstantSpeed()) {
+					// Swap the elements
+					Velocity temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp; 
+				}
+			}
+		}
+	}
+}
